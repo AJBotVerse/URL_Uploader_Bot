ffa3cfaac982912192710b6c56f8202823d5bbf3
AJTimePyro <ajtimepyro@gmail.com>
2021-09-30 22:10:01 +0530

    Fixed enivroment variable problem

--- bot/credentials.py
+++ bot/credentials.py
@@ -7,8 +7,8 @@
 import os
 
 
-bot_token = os.environ("BOT_TOKEN")
+bot_token = os.environ["BOT_TOKEN"]
 
-api_id = os.environ("API_ID")
+api_id = os.environ["API_ID"]
 
-api_hash = os.environ("API_HASH")
+api_hash = os.environ["API_HASH"]
